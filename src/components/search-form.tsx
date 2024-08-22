927fe6ac99f564b64c6893e20bb37615cae6e007
Bilal Khan <bilal.khan@live.com>
2024-08-22 12:28:34 +1000

    useDebounce hook for searchText

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,14 +1,28 @@
+"use client";
+import { useDebounce } from "@/hooks/hooks";
+import { useEffect, useState } from "react";
+import { setTimeout } from "timers";
+
 type SearchFormProps = {};
 
 function SearchForm(props: SearchFormProps) {
+  const [searchText, setSearchText] = useState("");
+  const debouncedText = useDebounce(searchText);
   return (
     <>
       <form className="w-full h-full" action="">
         <input
-          type="text"
-          className="w-full h-full bg-accent-light rounded-sm"
+          type="search"
+          className="w-full h-full bg-white/20 rounded-md px-5 outline-none 
+          focus:bg-white/50 hover:bg-white/30 placeholder-white/50
+          transition"
+          placeholder="Search pets..."
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
         />
       </form>
+      {debouncedText}
     </>
   );
 }
--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,8 +1,43 @@
 import { PetsContext } from "@/context/pets-context-provider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
+
+/**
+ * Custom hook to access the PetsContext.
+ * 
+ * This hook provides a convenient way to access the PetsContext within your components.
+ * It ensures that the context is available and throws an error if it is not.
+ * 
+ * @returns {PetsContextType} - The current value of the PetsContext.
+ * @throws {Error} - Throws an error if the PetsContext is not available.
+ */
 export function usePetContext() {
   const context = useContext(PetsContext)
   if (!context) throw new Error("There is no pets context")
   return context
+}
+
+/**
+ * Custom hook to debounce a given input text.
+ * 
+ * This hook delays the update of the input text by a specified delay time.
+ * It is useful for scenarios where you want to limit the rate at which a function is executed,
+ * such as handling user input in a search bar.
+ * 
+ * @param {string} inputText - The input text to be debounced.
+ * @param {number} [delay=500] - The delay time in milliseconds. Default is 500ms.
+ * @returns {string | null} - The debounced text.
+ */
+
+export function useDebounce(inputText: string, delay = 500) {
+  const [debouncedText, setDebouncedText] = useState<string | null>(null);
+
+  useEffect(() => {
+   const timer =  setTimeout(() => {
+
+      setDebouncedText(inputText);
+    }, delay);
+  return () => clearTimeout(timer)
+  }, [setDebouncedText, delay, inputText]);
+  return debouncedText;
 }
